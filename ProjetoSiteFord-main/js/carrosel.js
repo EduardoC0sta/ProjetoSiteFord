22fdd98bf47aae9f8824b724f7be0f15050a8a9b
EduardoC0sta <eduardocosttadias@gmail.com>
2025-05-28 11:34:15 -0300

    atualização na pagina sobre nós

--- ProjetoSiteFord-main/js/carrosel.js
+++ ProjetoSiteFord-main/js/carrosel.js
@@ -22,13 +22,12 @@ let Carrossel = {
 
     start: function (itens) {
         this.itens = itens;
-        this.container = document.querySelector(".carrossel");
         this.tituloContainer = document.querySelector(".titulo_carrossel");
         this.radioButtons = document.querySelectorAll('input[name="botao_carrossel"]');
-        
+
         // Configurar eventos para os botões de rádio
         this.radioButtons.forEach((radio, index) => {
-            radio.addEventListener('change', () => {
+            radio.addEventListener('change', ( ) => {
                 if (radio.checked) {
                     this.index = index;
                     this.renderItem(this.index);
@@ -55,24 +54,19 @@ let Carrossel = {
     },
 
     renderItem: function (index) {
-        this.container.innerHTML = "";
         this.tituloContainer.innerHTML = "";
 
         const item = this.itens[index];
 
-        const img = document.createElement("img");
-        img.src = item.imagem;
-        img.alt = item.titulo;
-
-        const link = document.createElement("a");
-        link.href = item.link;
-        link.appendChild(img);
-
-        this.container.appendChild(link);
-
         const p = document.createElement("p");
         p.textContent = item.titulo;
         this.tituloContainer.appendChild(p);
+
+        // Atualiza o background da div
+        const bg = document.querySelector(".background_carrossel");
+        bg.style.backgroundImage = `url('${item.imagem}')`;
+        bg.style.backgroundSize = "cover";
+        bg.style.backgroundPosition = "center";
     }
 };
 
@@ -80,10 +74,11 @@ let Carrossel = {
 let carrosselArr = [
     new CarrosselItem("img/ford-ranger-2024.jpg", "Nova Ford Ranger", "veiculos.html"),
     new CarrosselItem("img/ford-territory-2024.jpg", "Nova Ford Territory", "veiculos.html"),
-    new CarrosselItem("img/ford-raptor.jpg", "Nova Ford Raptor", "veiculos.html")
+    new CarrosselItem("img/ford-raptor.jpg", "Nova Ford Raptor", "veiculos.html"),
+    new CarrosselItem("img/celso.jpg", "Celso", "veiculos.html")
 ];
 
-//Iniciador do carrossel.
+// Iniciador do carrossel.
 window.addEventListener("DOMContentLoaded", () => {
     Carrossel.start(carrosselArr);
 });
